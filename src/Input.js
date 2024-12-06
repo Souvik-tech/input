338c5561acdc312b6e04f646895f615ca204219e
Souvik Das <souvikpc22@gmail.com>
2024-12-06 20:25:33 +0530

    form validation added

--- src/Input.js
+++ src/Input.js
@@ -5,17 +5,21 @@ const Input = () => {
     const [name,setName] = useState("");
     const [lastName,setLastName] = useState("");
     const [fullname,setFullname] = useState("");
-    const handleClick = () =>{
+    const handleClick = (e) =>{
+        e.preventDefault();
         setFullname(`${name} ${lastName}`)
     }
   return (
     <div>
-       <label for="name">Your Name</label> <input type='text' id='name' onChange={(e)=>setName(e.target.value)}/>
-       <br></br>
-       <label for="lastName">Last Name</label> <input type='text' id='lastName' onChange={(e)=>setLastName(e.target.value)}/>
-       <br>
-       </br>
-       <button onClick={handleClick}>Submit</button>
+        <form onSubmit={handleClick}>
+
+            <label for="name">Your Name</label> <input required type='text' value={name} id='name' onChange={(e)=>setName(e.target.value)}/>
+            <br></br>
+            <label for="lastName">Last Name</label> <input required type='text' id='lastName' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+            <br>
+            </br>
+            <button type='submit'>Submit</button>
+        </form>
        {fullname && (
         <p style={{ marginTop: "20px", fontWeight: "bold" }}>
           Full Name: {fullname}
