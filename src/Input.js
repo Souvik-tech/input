eb5fdddc9715dc147bf5e7bf89efa9c73e2e0159
Souvik Das <souvikpc22@gmail.com>
2024-12-06 20:47:06 +0530

    initial checks

--- src/Input.js
+++ src/Input.js
@@ -11,11 +11,12 @@ const Input = () => {
     }
   return (
     <div>
+        <h1>Full Name Display</h1>
         <form onSubmit={handleClick}>
 
-            <label for="name">Your Name</label> <input required type='text' value={name} id='name' onChange={(e)=>setName(e.target.value)}/>
+            <label for="name">Your Name:</label> <input required type='text' value={name} id='name' onChange={(e)=>setName(e.target.value)}/>
             <br></br>
-            <label for="lastName">Last Name</label> <input required type='text' id='lastName' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+            <label for="lastName">Last Name:</label> <input required type='text' id='lastName' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
             <br>
             </br>
             <button type='submit'>Submit</button>
